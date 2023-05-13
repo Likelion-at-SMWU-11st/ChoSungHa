47c511205dd52ca33fc10125604673ecbd61d3de
Cho Sungha <earl9rey@naver.com>
2023-05-13 15:43:45 +0900

    22번

--- 4th_session/weather.py
+++ 4th_session/weather.py
@@ -3,10 +3,11 @@ import json
 
 city = "Seoul"
 apikey = "40c6ed48210c8f9c624ede2f8d2753af"
+
 api = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apikey}"
 
 result = requests.get(api)
-print(result.text)
+#print(result.text)
 
 data = json.loads(result.text)
 
@@ -14,7 +15,7 @@ data = json.loads(result.text)
 # print(type(data))
 
 print(data["name"], "의 날씨입니다.")
-print("날씨는 ", data["weather"][0]["main"], "입니다.")
+print("날씨는 ", data["weather"][0]["description"], "입니다.")
 print("현재 온도는",data["main"]["temp"],"입니다.")
 print("하지만 체감 온도는 ",data["main"]["feels_like"], "입니다.")
 print("최저 기온은 ", data["main"]["temp_min"], "입니다.")
